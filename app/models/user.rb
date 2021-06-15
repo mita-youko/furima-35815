fb9bddbce5c0e2cd6fb04e14c94e3133aeadc8cc
mitachan-1 <youko.mita1027@gmail.com>
2021-06-15 20:40:26 +0900

    【訂正】with_optionsでコードをまとめました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,14 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
   with_options presence: true do
     validates :nickname
-    validates :last_name,          format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters'}
-    validates :first_name,         format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters'}
-    validates :last_name_kana,     format: {with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters'}
-    validates :first_name_kana,    format: {with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters'}
+    with_options format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters'} do
+     validates :last_name
+     validates :first_name
+    end
+    with_options format: {with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters'} do
+     validates :last_name_kana
+     validates :first_name_kana
+    end
     validates :birthday_id       
   end
 end
