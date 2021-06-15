3ffa58ca07851272213d84f0193fc53a0c861b24
mitachan-1 <youko.mita1027@gmail.com>
2021-06-15 20:57:28 +0900

    【訂正】「々」と「全角ハイフン」を許可するように変更しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
   with_options presence: true do
     validates :nickname
-    with_options format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters'} do
+    with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters'} do
      validates :last_name
      validates :first_name
     end
