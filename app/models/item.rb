390be214b6a8e3463aab987a5a8942a57cc56dfd
mitachan-1 <youko.mita1027@gmail.com>
2021-06-16 15:04:45 +0900

    ActiveHashを導入し、バリデーションを設定しました。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :ship_cost
+  belongs_to :prefecture
+  belongs_to :ship_date
+
+  with_options presence: true do
+    validates :category_id
+    validates :condition_id
+    validates :ship_cost_id
+    validates :prefecture
+    validates :ship_date_id
+  end
+
 end
