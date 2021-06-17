b44eea2cdb8a3f10a6777322ae2b9a62d17fcff9
mitachan-1 <youko.mita1027@gmail.com>
2021-06-17 16:53:08 +0900

    単体テストコードを作成しました。

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,13 @@
 FactoryBot.define do
   factory :item do
+    name              { "アイテム" }
+    description       { Faker::Lorem.sentence}
+    category_id       { '3' }
+    condition_id      { '3' }
+    ship_cost_id      { '3' }
+    prefecture_id     { '3' }
+    ship_date_id      { '3' }
+    price             { '300' }
+    association :user
   end
 end
