e4d101af34b545267a4107f9f5bbdf6164cd34be
mitachan-1 <youko.mita1027@gmail.com>
2021-06-17 18:55:23 +0900

    【修正】console.logを削除しました。

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,16 +2,13 @@ window.addEventListener('load', () => {
     const priceInput = document.getElementById("item-price");
     priceInput.addEventListener("input", () => {
         const inputValue = priceInput.value;
-        console.log(inputValue);
 
         const addTaxDom = document.getElementById("add-tax-price");
         addTaxDom.innerHTML = (Math.floor(inputValue*0.1));
-        console.log(addTaxDom)
 
-        const profitNum = document.getElementById("profit") 
-        resultNum = inputValue*0.1
-        profitNum.innerHTML = (Math.floor(inputValue-resultNum))
-        console.log(profitNum)
+        const profitNum = document.getElementById("profit"); 
+        resultNum = inputValue*0.1;
+        profitNum.innerHTML = (Math.floor(inputValue-resultNum));
     })
     
 });
