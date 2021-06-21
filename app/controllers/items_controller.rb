edb69aedc563fcb3efe96e47094edd0bb1ce3b12
mitachan-1 <youko.mita1027@gmail.com>
2021-06-21 17:20:31 +0900

    商品の削除が行われたら、トップページへ戻るように仮設定しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :update, :show]
+  before_action :set_item, only: [:edit, :update, :show, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only:[:edit, :update]
   def index
@@ -34,6 +34,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if @item.destroy
+      redirect_to root_path
+    end
   end
 
   private
