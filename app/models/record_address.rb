633de966e50018c5dbd97ed78e01e424143a0425
mitachan-1 <youko.mita1027@gmail.com>
2021-06-22 20:15:53 +0900

    【修正】２点修正しました。
    ・record_addressモデルのバリデーションを修正しました
    ・addressesテーブルに保存するものを追加しました

--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -5,7 +5,6 @@ class RecordAddress
     with_options presence: true do
       validates :user_id
       validates :item_id
-      validates :record_id
       validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
       validates :city
       validates :house_number
@@ -16,6 +15,6 @@ class RecordAddress
 
     def save
         record = Record.create(user_id: user_id, item_id: item_id)
-        Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, record_id: record.id)
+        Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, record_id: record.id)
     end
 end
