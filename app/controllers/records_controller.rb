d764dbabeae19f332c4e770fa1933d67a98cb201
mitachan-1 <youko.mita1027@gmail.com>
2021-06-22 18:40:24 +0900

    商品購入に関するビューを編集しました。

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,9 +2,23 @@ class RecordsController < ApplicationController
 
     def index
         @record_address = RecordAddress.new
+        @item = Item.find(params[:item_id])
     end
 
     def create
+        @record_address = RecordAddress.new(record_params)
+        if @record_address.valid?
+            @record_address.save
+            redirect_to root_path
+        else
+            render :index
+        end
+    end
+
+
+    private
         
+    def record_params
+        params.require(:record_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
     end
 end
