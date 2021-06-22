90b25e6f8a1f97b9a4d7e8c0a85828f4561d78c9
mitachan-1 <youko.mita1027@gmail.com>
2021-06-22 21:47:48 +0900

    ログイン状態でも、自身が出品した売却済み商品の編集ページへ遷移できないようにしました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,7 +51,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless current_user.id == @item.user_id
+    unless current_user.id == @item.user_id && @item.record.nil?
       redirect_to root_path
     end
   end
