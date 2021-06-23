5fcea025012a2a70ca702023af68e4b033c0f5d1
mitachan-1 <youko.mita1027@gmail.com>
2021-06-23 19:00:48 +0900

    バリデーションを追加しました。

--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -8,10 +8,9 @@ class RecordAddress
       validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
       validates :city
       validates :house_number
-      validates :phone_number
+      validates :phone_number, format: {with: /\A\d{10,11}\z/}
     end
     validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
-    validates :phone_number, numericality: true
 
     def save
         record = Record.create(user_id: user_id, item_id: item_id)
