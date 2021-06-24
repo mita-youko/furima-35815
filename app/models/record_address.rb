09f68f64a99373f20153a8b34e10fba88659cd30
mitachan-1 <youko.mita1027@gmail.com>
2021-06-24 17:58:16 +0900

    tokenに関するバリデーションを追加しました。

--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -9,6 +9,7 @@ class RecordAddress
       validates :city
       validates :house_number
       validates :phone_number, format: {with: /\A\d{10,11}\z/}
+      validates :token
     end
     validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
