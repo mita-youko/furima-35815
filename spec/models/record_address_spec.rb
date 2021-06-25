f4c3ebf31751fc9c107b2eb28bcc29cf5c3df2c8
mitachan-1 <youko.mita1027@gmail.com>
2021-06-25 14:57:44 +0900

    【修正５】単体テストコードを修正しました。

--- spec/models/record_address_spec.rb
+++ spec/models/record_address_spec.rb
@@ -81,16 +81,17 @@ RSpec.describe RecordAddress, type: :model do
         @record_address.valid?
         expect(@record_address.errors.full_messages).to include('Phone number is invalid')
       end
+
+      it 'user_idが空だと保存できない' do
+        @record_address.user_id = ''
+        @record_address.valid?
+        expect(@record_address.errors.full_messages).to include("User can't be blank")
+      end
+      it 'item_idが空だと保存できない' do
+        @record_address.item_id = ''
+        @record_address.valid?
+        expect(@record_address.errors.full_messages).to include("Item can't be blank")
+      end
     end
   end
 end
-
-# 配送先の住所情報も購入の都度入力させること。
-# 郵便番号が必須であること。
-# 郵便番号は、「3桁ハイフン4桁」の半角文字列のみ保存可能なこと（良い例：123-4567　良くない例：1234567）。
-# 都道府県が必須であること。
-# 市区町村が必須であること。
-# 番地が必須であること。
-# 建物名は任意であること。
-# 電話番号が必須であること。
-# 電話番号は、10桁以上11桁以内の半角数値のみ保存可能なこと（良い例：09012345678　良くない例：090-1234-5678）。
